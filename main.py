2596d8192ff9adff14d5fd477cab32f8c3ae198e
joweid <johannes-weidmann@web.de>
2023-04-22 21:24:07 +0200

    parse queries from topics.xml

--- main.py
+++ main.py
@@ -3,13 +3,8 @@ from revChatGPT.V1 import Chatbot
 import json
 
 from itertools import islice, tee
+import xml.etree.ElementTree as ET
 
-
-def jaccard(query, text): 
-    query_4_grams = set(list(zip(*(islice(seq, index, None) for index, seq in enumerate(tee(query.lower(), 4))))))
-    text_4_grams = set(list(zip(*(islice(seq, index, None) for index, seq in enumerate(tee(text.lower(), 4))))))
-    
-    return float(len(query_4_grams.intersection(text_4_grams)) / len(query_4_grams.union(text_4_grams)))
 
 def retrieve_texts_matching_to_query(query):
     file = open("arguments.json")
@@ -20,19 +15,31 @@ def retrieve_texts_matching_to_query(query):
         text = line['text']
         original_query = line['query']
 
-        #if (jaccard(keyword, text.lower()) > 0.05 or jaccard(keyword, original_query) > 0.05) and len(text) > 200:
-        #    relevant_texts.append(text)
-        if jaccard(query, text) > 0.05:
+        if original_query.lower() == query.lower():
             relevant_texts.append(text)
         
-    return relevant_texts   
+    return relevant_texts  
 
+
+def parse_topics():
+    xml_tree = ET.parse('topics.xml')
+    root = xml_tree.getroot()
+    topics = []
+    
+    for topic in root.findall('topic'):
+        query = topic.find('title').text
+        topics.append(query)
+    
+    return topics
+    
 def main():
     chatbot = Chatbot(config=
         json.load(open("access_token.json"))
     )
 
-    query = "Are genetically modified foods healthy?"
+    queries = parse_topics()
+
+    """query = "Are genetically modified foods healthy?"
     texts = retrieve_texts_matching_to_query(query)
 
     
@@ -41,7 +48,7 @@ def main():
         print(f"{text} \n\n")
         for data in chatbot.ask(f"This is a text: \"{text}\". Return arguments that you can inference from this text matching to this query: \"{query}\". You should only give me arguments that can be inferred fom the text. Give me the arguments in a list split in PRO and CONTRA arguments and please give me the line for each argument from where it can be inferenced."):
             response = data["message"]
-        print(f"{response}\n\n\n")
+        print(f"{response}\n\n\n")"""
     
 
 if __name__ == "__main__":
