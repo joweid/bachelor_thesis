dd2edd97a74cd990def27b95c2395554fd3830a6
joweid <johannes-weidmann@web.de>
2023-04-24 23:54:37 +0200

    retrieve five best arguments from all relevant texts

--- main.py
+++ main.py
@@ -57,6 +57,44 @@ def n_most_relevant(most_similar_indices, corpus, limit):
     return n_most_relevant
 
 
+def split_chatgpt_args(gpt_args, chunk_size):
+    for i in range(0, len(gpt_args), chunk_size):
+        yield gpt_args[i:i + chunk_size]
+
+
+def n_best_chatgpt_arguments(texts, query, chatbot, n):
+    arguments = []
+    for text in texts:
+        arguments.append(retrieve_arguments_with_chatgpt(query, text, chatbot))
+
+    first_round = list(split_chatgpt_args(arguments, 4))
+    candidates = []
+    i = 1
+    j = 1
+    print("First Round: \n")
+    for section in first_round:
+        print(f"{i} / {len(first_round)}")
+        i += 1
+        for data in chatbot.ask(f"These are some arguments you gave me: {str(section)}. Give the {n} best arguments that fit the most to this query: {query}"):
+            response = data["message"]
+        candidates.append(response)
+    
+    second_round = list(split_chatgpt_args(candidates, 3))
+    finalists = []
+    print("Second Round: \n")
+    for section in second_round:
+        print(f"{j} / {len(second_round)}")
+        j += 1
+        for data in chatbot.ask(f"These are some arguments you gave me: {str(section)}. Give the {n} best arguments that fit the most to this query: {query}"):
+            response = data["message"]
+        finalists.append(response)
+    
+    for data in chatbot.ask(f"These are some arguments you gave me: {str(finalists)}. Give the {n} best arguments that fit the most to this query: {query}"):
+        response = data["message"]
+    
+    return response
+    
+
 def retrieve_arguments_with_jaccard(query, text):
     sentences = sent_tokenize(text)
     arguments = []
@@ -112,12 +150,15 @@ def retrieve_arguments_with_tfidf(query, text):
 
 
 def retrieve_arguments_with_chatgpt(query, text, chatbot):
-    for data in chatbot.ask(f"This is a text: \"{text}\". Return arguments that you can inference from this text matching to this query: \"{query}\". You should only give me arguments that can be inferred fom the text. Give me the arguments in a list split in PRO and CONTRA arguments and please give me the line for each argument from where it can be inferenced."):
-        response = data["message"]
+    try:
+        for data in chatbot.ask(f"This is a text: \"{text}\". Return arguments that you can inference from this text matching to this query: \"{query}\". You should only give me arguments that can be inferred fom the text. Give me the arguments in a list split in PRO and CONTRA arguments and please give me the line for each argument from where it can be inferenced."):
+            response = data["message"]
+    except:
+        response = ""
+    finally:
+        return response
     
-    return response
-
-
+    
 def main():
     # Initialize ChatBot for ChatGPT
     chatbot = Chatbot(config=
@@ -127,22 +168,19 @@ def main():
     # Initialize model for BERT
     model = SentenceTransformer('all-MiniLM-L6-v2')
 
-    queries = parse_topics()
+    queries = parse_topics()[0:30]
+    gpt_args = []
+    i = 0
     for query in queries:
         relevant_texts = retrieve_texts_matching_to_query(query)
-        for text in relevant_texts:
-            #print(f"Text: \n{text}\n\n")
-            jaccard_args = retrieve_arguments_with_jaccard(query, text)
-            print(f"Jaccard: {jaccard_args}\n\n")
-            bm25_args = retrieve_arguments_with_bm25(query, text)
-            print(f"BM25: {bm25_args}\n\n")
-            bert_args = retrieve_arguments_with_bert(query, text, model)
-            print(f"BERT: {bert_args}\n\n")
-            tfidf_args = retrieve_arguments_with_tfidf(query, text)
-            print(f"TF-IDF: {tfidf_args}\n\n")
-            chatgpt_args = retrieve_arguments_with_chatgpt(query, text, chatbot)
-            print(f"GPT: {chatgpt_args}\n\n\n")
-    
+        joined_texts = " ".join(map(str, relevant_texts))
+
+        jaccard_args = retrieve_arguments_with_jaccard(query, joined_texts)
+        bm25_args = retrieve_arguments_with_bm25(query, joined_texts)
+        bert_args = retrieve_arguments_with_bert(query, joined_texts, model)
+        tfidf_args = retrieve_arguments_with_tfidf(query, joined_texts)
+        gpt_args = n_best_chatgpt_arguments(relevant_texts, query, chatbot, 5)
+
 
 if __name__ == "__main__":
     main()
